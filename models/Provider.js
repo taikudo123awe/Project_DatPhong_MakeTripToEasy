d44da709774380a7e3049c1229cfc16c5fd13aae
PhamViKhang <21057711.khang@student.iuh.edu.vn>
2025-10-20 06:42:49 +0700

    sai ở chỗ khai báo include khi join model Provider và đã cập nhật lại

--- models/Provider.js
+++ models/Provider.js
@@ -1,45 +1,48 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-const Account = require('./Account');
-// Xóa "const PaymentInfo = require('./PaymentInfo');" nếu bạn đã thêm ở lần trước
-// Xóa quan hệ với PaymentInfo ở cuối file nếu bạn đã thêm ở lần trước
+// models/Provider.js
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+const Account = require("./Account");
 
-const Provider = sequelize.define('Provider', {
-  providerId: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
+const Provider = sequelize.define(
+  "Provider",
+  {
+    providerId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    providerName: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+    },
+    taxCode: {
+      type: DataTypes.STRING(20),
+    },
+    accountId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Account",
+        key: "accountId",
+      },
+    },
   },
-  providerName: {
-    type: DataTypes.STRING(50), // Sửa lại đúng schema
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING(255),
-    allowNull: false
-  },
-  phoneNumber: {
-    type: DataTypes.STRING(15),
-    allowNull: false
-  },
-  taxCode: {
-    type: DataTypes.STRING(20) // Sửa lại đúng schema
-  },
-  accountId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'Account',
-      key: 'accountId'
-    }
+  {
+    tableName: "Provider",
+    timestamps: false,
   }
-}, {
-  tableName: 'Provider',
-  timestamps: false
-});
+);
 
 // Quan hệ 1-1 với Account
-Provider.belongsTo(Account, { foreignKey: 'accountId' });
-Account.hasOne(Provider, { foreignKey: 'accountId' });
+Provider.belongsTo(Account, { foreignKey: "accountId" });
+Account.hasOne(Provider, { foreignKey: "accountId" });
 
 module.exports = Provider;
